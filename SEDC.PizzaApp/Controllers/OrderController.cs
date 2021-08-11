640b920a2b78bc875a05e1bb6cb9592034564d54
jovanamiskimovska <jovana.miskimovska@gmail.com>
2021-08-11 02:11:52 +0200

    SEDC.PizzaApp.Version 1

---
+++ SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Order> orders = StaticDb.Orders;
+            return View(orders);
+        }
+
+        [Route("Order/Details/{id?}")]
+        public IActionResult Details (int? id)
+        {
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+            else
+            {
+                Order order = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+                if(order == null)
+                {
+                    return new EmptyResult();
+                }
+                else
+                {
+                    return View(order);
+                }
+            }
+        }
+        [Route("Order/JsonData")]
+
+        public IActionResult JsonData()
+        {
+            Order newOrder = new Order()
+            {
+                Id = 3,
+                Pizza = StaticDb.Pizzas.Last(),
+                User = StaticDb.Users.First(),
+                PaymentMethod = Models.Enums.PaymentMethod.Cash
+            };
+            return new JsonResult(newOrder);
+        }
+
+        public IActionResult RedirectToHomeController()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.PizzaApp.Models.Domain;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            return View(pizzas); 
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if (pizza == null)
+            {
+                return new EmptyResult();
+            }
+            return View(pizza);
+        }
+        [Route("SeePizzas")] 
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
