77fa4963fa2c832ec3181c03bb39b8feeb2099ea
jovanamiskimovska <jovana.miskimovska@gmail.com>
2021-09-05 13:38:13 +0200

    Refactored

--- SEDC.PizzaApp.Refactor/SEDC.PizzaApp.Refactored/Startup.cs
+++ SEDC.PizzaApp.Refactor/SEDC.PizzaApp.Refactored/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SEDC.PizzaApp.Helpers;
+using SEDC.PizzaApp.Services.Implementations;
+using SEDC.PizzaApp.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,13 +22,14 @@ namespace SEDC.PizzaApp.Refactored
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            DependencyInjectionHelper.InjectRepositories(services);
+            DependencyInjectionHelper.InjectServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,10 +39,7 @@ namespace SEDC.PizzaApp.Refactored
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
